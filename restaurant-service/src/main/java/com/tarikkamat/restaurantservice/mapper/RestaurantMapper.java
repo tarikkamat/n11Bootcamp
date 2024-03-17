35d6e2b3491e9991de77e58660246eb446b692b4
tarikkamat <tarikkamat2@gmail.com>
2024-03-17 06:33:40 +0300

    RestaurantMapper created

---
+++ restaurant-service/src/main/java/com/tarikkamat/restaurantservice/mapper/RestaurantMapper.java
@@ -0,0 +1,18 @@
+package com.tarikkamat.restaurantservice.mapper;
+
+import com.tarikkamat.restaurantservice.dto.RestaurantDTO;
+import com.tarikkamat.restaurantservice.entity.Restaurant;
+import com.tarikkamat.restaurantservice.request.RestaurantRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    RestaurantDTO convertToRestaurantDTO(Restaurant restaurant);
+    Restaurant convertToRestaurant(RestaurantRequest request);
+
+}
